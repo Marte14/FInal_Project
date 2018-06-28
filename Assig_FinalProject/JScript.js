16bb38d3ca7489d85d3e477b174e302b0cc7da1c
marte14 <kattymarte14@gmail.com>
2018-06-28 09:37:32 -0400

    Final Project

--- Assig_FinalProject/JScript.js
+++ Assig_FinalProject/JScript.js
@@ -1,8 +1,20 @@
+		// document.addEventListener('loadend',loadBooklist());
+		
+		document.getElementById('bookList').innerHTML = ''; 
+ 
+		function loadBooklist(){
+			for(var i=0; i<booklist.length; i++){
+			document.getElementById('bookList').innerHTML +='<li> Book Title: ' + 
+			booklist[i].title +'Author: ' + booklist[i].author +'<br> Index Number: ' + 
+			booklist[i].bookId + 'Total of Books: ' + booklist[i].totalBooks +'</li>';
 
-
+			}
+			console.log(booklist);
+		}
 		document.getElementById("date").innerHTML = TodaysDate();
-
+		
 		function TodaysDate() {
+		
 		var date = new Date(),
 		    minutes = date.getMinutes().toString() == 1 ? '0'+date.getMinutes() : date.getMinutes(),
 		    hours = date.getHours().toString().length == 1 ? '0'+date.getHours() : date.getHours(),
@@ -13,20 +25,20 @@
 		    months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
 		    days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
 		
-			return days[date.getDay()]+' '+months[date.getMonth()]+' '+date.getDate()+' '+date.getFullYear()+'<br> ' + hours+':'+minutes+ampm;
+			return days[date.getDay()]+' '+months[date.getMonth()]+' '+date.getDate()+' '+date.getFullYear()+'<br> ' + 
+			hours+':'+minutes+ " " + ampm;
 		}
 
 
 		var booklist = [
-		{title:'The Great Gatsby', author: 'F. Scott Fitzgerald', bookId: 2, totalBooks: 9},
-		{title:'The Grapes of Wrath', author: 'John Steinbeck', bookId: 3, totalBooks: 5},
-		{title:'Ulysses', author: 'James Joyce', bookId: 4, totalBooks: 13},
-		{title:'Nineteen Eighty-Four', author: 'John Steinbeck', bookId: 5, totalBooks: 11},
- 		{title:'Lolita', author: 'Vladimir Nabokov', bookId: 6, totalBooks: 12},
- 		{title:'Catch-22', author: 'by Joseph Helle', bookId: 7, totalBooks: 10},
-		
-
+		{title:'The Great Gatsby', author: 'F. Scott Fitzgerald', bookId: 1, totalBooks: 9},
+		{title:'The Grapes of Wrath', author: 'John Steinbeck', bookId: 2, totalBooks: 5},
+		{title:'Ulysses', author: 'James Joyce', bookId:3, totalBooks: 13},
+		{title:'Nineteen Eighty-Four', author: 'John Steinbeck', bookId: 4, totalBooks: 11},
+ 		{title:'Lolita', author: 'Vladimir Nabokov', bookId: 5, totalBooks: 12},
+ 		{title:'Catch-22', author: 'by Joseph Helle', bookId: 6, totalBooks: 10}
 		];
+		
 		function book(){
 			return{
 				title: document.getElementById('title').value.trim(),
@@ -40,7 +52,6 @@
 					return this.totalBooks--;
 
 				}
-
 			}
 		} 
 
@@ -52,52 +63,58 @@
 			displayBooklist();
 
 		}
+		
+		
+
 		function displayBooklist(){
 			document.getElementById('bookList').innerHTML = ''; 
-
-			//element to hold text. 
-			for(var i=0; i<booklist.length; i++){// this i is that element 
-				document.getElementById('bookList').innerHTML += '<li>Book Title: ' +booklist[i].title+'<br>Author: ' + booklist[i].author +'<br> Index Number: ' + booklist[i].bookId +'<br>' + 'Total of Books: ' + booklist[i].totalBooks +'</li><br>';
+				for(var i=0; i<booklist.length; i++){
+					document.getElementById('bookList').innerHTML += '<li>Book Title: ' +booklist[i].title+'<br>Author: ' +
+					booklist[i].author +'<br> Index Number: ' + booklist[i].bookId +'<br>' + 'Total of Books: ' + 
+					booklist[i].totalBooks +'</li><br>';
 			}
-		}
+		}		console.log(booklist);
 
-		function searchData() {
-			 document.getElementById('bkListSearch').innerHTML = ''; 
 
- 			for (var i=0; i< booklist.length ; i++) {
-				if (booklist[i].title == document.getElementById('title').value.trim()) {
-    			
-    				document.getElementById('bkListSearch').innerHTML +='<li> Book Title: ' + booklist[i].title +'<br>Author: ' + booklist[i].author +'<br> Index Number: ' + booklist[i].bookId +'<br>' + 'Total of Books: ' + booklist[i].totalBooks +'</li><br>';
-    					//return book.title;
 
-  				}
-  		
-  				
- 	
- 			}
- 		//console.log(book(title));
 
-		
-		}
-
-			function checkOut(bookId,int){
+		function checkOut(bookId,int){
 				var num;
 				for (var i=0; i< booklist.length ; i++) {
 					if (booklist[i].title == document.getElementById('titleOut').value.trim()) {
 						num = i;
 
-					}
-				}
+			}
+		}
 				if (num != undefined){
-					 console.log(num)
+					console.log(num)
 					booklist[num].totalBooks = booklist[num].totalBooks - document.getElementById('quantity').value;
-					document.getElementById('bkListOut').innerHTML = '<br>' + booklist[num].title + 'has been Checked Out by: ' + booklist[num].borrower + '!. Total Of This Title Available Now: ' + booklist[num].totalBooks ;	
+					document.getElementById('bkListOut').innerHTML = '<br>' + booklist[num].title + 'has been Checked Out by: ' +
+					booklist[num].borrower + '!. Total Of This Title Available Now: ' + booklist[num].totalBooks ;	
 
 				}else{
 					alert(document.getElementById('titleOut').value + " Is Not Available!");
-				}
-				
-				
-				
-
 			}
+		}
+
+		// function searchData() {
+		// 	 document.getElementById('bkListSearch').innerHTML = ''; 
+
+ 	// 		for (var i=0; i< booklist.length ; i++) {
+		// 		if (booklist[i].title == document.getElementById('title').value.trim()) {
+    			
+  //   				document.getElementById('bkListSearch').innerHTML +='<li> Book Title: ' + booklist[i].title +
+  //   				'<br>Author: ' + booklist[i].author +'Index Number: ' + booklist[i].bookId +'<br>' + 'The Total of Books Available Is: ' +
+  //   				 booklist[i].totalBooks +'</li><br>';
+  //   					//return book.title;
+
+  // 				}
+  		
+  				
+ 	
+ 	// 		}
+ 	// 	//console.log(book(title));
+
+		
+		// }
+			
